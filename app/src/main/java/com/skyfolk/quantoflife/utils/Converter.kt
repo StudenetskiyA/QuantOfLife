7ca418e1a9f1dca21fbc9dbabe40197ab3290992
StudenetskyA <wow.dayre@gmail.com>
2021-05-12 22:05:59 +0500

    Конвертер временного интервала

--- app/src/main/java/com/skyfolk/quantoflife/utils/Converter.kt
+++ app/src/main/java/com/skyfolk/quantoflife/utils/Converter.kt
@@ -1,5 +1,7 @@
 package com.skyfolk.quantoflife.utils
+
 import com.skyfolk.quantoflife.entity.QuantCategory
+import com.skyfolk.quantoflife.ui.feeds.TimeInterval
 import java.time.Instant
 import java.time.ZoneId
 import java.time.ZonedDateTime
@@ -7,33 +9,41 @@ import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 import java.util.*
 
-fun Long.toDate() : String {
+fun Long.toDate(): String {
     return DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM, FormatStyle.SHORT)
         .withLocale(Locale("ru"))
         .format(
             ZonedDateTime.ofInstant(
                 Instant.ofEpochMilli(this),
-                ZoneId.systemDefault()))
+                ZoneId.systemDefault()
+            )
+        )
 }
-fun Long.toDateWithoutHourAndMinutes() : String {
+
+fun Long.toDateWithoutHourAndMinutes(): String {
     return DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).format(
-            ZonedDateTime.ofInstant(
-                Instant.ofEpochMilli(this),
-                ZoneId.systemDefault()))
+        ZonedDateTime.ofInstant(
+            Instant.ofEpochMilli(this),
+            ZoneId.systemDefault()
+        )
+    )
 }
-fun Long.toShortDate() : String {
+
+fun Long.toShortDate(): String {
     val calendar = Calendar.getInstance()
     calendar.timeInMillis = this
-    val day = if (calendar[Calendar.DAY_OF_MONTH] + 1 < 10) "0${calendar[Calendar.DAY_OF_MONTH]}" else calendar[Calendar.DAY_OF_MONTH]
-    val month = if (calendar[Calendar.MONTH] +1 < 10) "0${calendar[Calendar.MONTH]+1}" else calendar[Calendar.MONTH]+1
+    val day =
+        if (calendar[Calendar.DAY_OF_MONTH] + 1 < 10) "0${calendar[Calendar.DAY_OF_MONTH]}" else calendar[Calendar.DAY_OF_MONTH]
+    val month =
+        if (calendar[Calendar.MONTH] + 1 < 10) "0${calendar[Calendar.MONTH] + 1}" else calendar[Calendar.MONTH] + 1
     return "$day:$month"
 }
 
-fun Long.toCalendarOnlyHourAndMinute() : Calendar {
+fun Long.toCalendarOnlyHourAndMinute(): Calendar {
     val calendar = Calendar.getInstance()
     calendar.timeInMillis = this
-    val hour: Int = (this/(60*60*1000)).toInt()
-    val minute: Int = (this/(60*1000)%60).toInt()
+    val hour: Int = (this / (60 * 60 * 1000)).toInt()
+    val minute: Int = (this / (60 * 1000) % 60).toInt()
 
     calendar[Calendar.HOUR_OF_DAY] = hour
     calendar[Calendar.MINUTE] = minute
@@ -41,8 +51,46 @@ fun Long.toCalendarOnlyHourAndMinute() : Calendar {
     return calendar
 }
 
-fun Long.toCalendar() : Calendar {
+fun Long.toCalendar(): Calendar {
     val calendar = Calendar.getInstance()
     calendar.timeInMillis = this
     return calendar
+}
+
+fun fromPositionToTimeInterval(
+    position: Int,
+    startIntervalCalendar: Long,
+    endIntervalCalendar: Long
+): TimeInterval {
+    return when (position) {
+        0 -> {
+            TimeInterval.Today
+        }
+        1 -> {
+            TimeInterval.Week
+        }
+        2 -> {
+            TimeInterval.Month
+        }
+        3 -> {
+            TimeInterval.All
+        }
+        4 -> {
+            TimeInterval.Selected(
+                startIntervalCalendar,
+                endIntervalCalendar
+            )
+        }
+        else -> TimeInterval.All
+    }
+}
+
+fun TimeInterval.toPosition(): Int {
+    return when (this) {
+        is TimeInterval.Today -> 0
+        is TimeInterval.Week -> 1
+        is TimeInterval.Month -> 2
+        is TimeInterval.All -> 3
+        is TimeInterval.Selected -> 4
+    }
 }
